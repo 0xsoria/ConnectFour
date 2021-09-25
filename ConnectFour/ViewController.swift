8df2b98a9f0b4fb3a0476e58e1a98bf4854be507
Gabriel Sória <gabrielss.rock@me.com>
2021-09-25 16:43:19 -0300

    Initial Commit

---
+++ ConnectFour/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ConnectFour
+//
+//  Created by Gabriel Soria Souza on 25/09/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
