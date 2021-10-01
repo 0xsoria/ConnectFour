259c2719cb05538fd1c340af1f41a5c03c901e5e
Gabriel Sória <gabrielss.rock@me.com>
2021-10-01 10:00:37 -0300

    Creates a grid view

--- ConnectFour/Views/MainView.swift
+++ ConnectFour/Views/MainView.swift
@@ -213,7 +213,7 @@ final class MainView: NSView {
         
         self.board.collectionViewLayout = flowLayout
         
-        let gridView = GridView(lines: 6, columns: 7)
+        let gridView = GridView(lines: numberOfLines, columns: numberOfColumns)
         self.board.backgroundView = gridView
         self.board.backgroundColors = [.white]
     }
@@ -260,64 +260,5 @@ extension MainView {
         case winnerYellow = "Yellow is the winner"
         case tie = "It's a tie"
         case connectFour = "Connect four coins of the same color horizontally, vertically or diagonally."
-    }
-}
-
-
-final class GridView: NSView {
-    private let path = NSBezierPath()
-    private let lines: Int
-    private let columns: Int
-    
-    init(lines: Int, columns: Int) {
-        self.lines = lines
-        self.columns = columns
-        super.init(frame: .zero)
-    }
-
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private var gridWidthMultiple: CGFloat {
-        return CGFloat(self.columns)
-    }
-    
-    private var gridHeightMultiple : CGFloat {
-        return CGFloat(self.lines)
-    }
-    
-    private var gridWidth: CGFloat {
-        return bounds.width/CGFloat(gridWidthMultiple)
-    }
-    
-    private var gridHeight: CGFloat {
-        return bounds.height/CGFloat(gridHeightMultiple)
-    }
-    
-    private func drawGrid() {
-        path.lineWidth = 5.0
-
-        for index in 0...Int(gridWidthMultiple) {
-            let start = CGPoint(x: CGFloat(index) * gridWidth, y: 0)
-            let end = CGPoint(x: CGFloat(index) * gridWidth, y:bounds.height)
-            path.move(to: start)
-            path.line(to: end)
-        }
-
-        for index in 0...Int(gridHeightMultiple) {
-            let start = CGPoint(x: 0, y: CGFloat(index) * gridHeight)
-            let end = CGPoint(x: bounds.width, y: CGFloat(index) * gridHeight)
-            path.move(to: start)
-            path.line(to: end)
-        }
-        path.close()
-    }
-    
-    override func draw(_ dirtyRect: NSRect) {
-        self.drawGrid()
-        NSColor.blue.setStroke()
-        path.stroke()
     }
 }
